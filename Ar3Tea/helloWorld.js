1535da7070a92eab8a0ed2e0af3c27a31d2ee107
Ar3Tea <tbusinessr@gmail.com>
2018-10-02 14:40:46 -0400

    My hello world in javascript

---
+++ Ar3Tea/helloWorld.js
@@ -0,0 +1,11 @@
+// This file goes through a couple different 'Hello World' scenarios for JavaScript
+
+// First Hello World
+console.log('Hello World!');
+
+// Second Hellow World
+const helloWorld = "Hello World!";
+console.log(helloWorld);
+
+// Third Hello World
+alert('Hello World!');
